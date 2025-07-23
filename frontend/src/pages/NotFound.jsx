63101b8692eb4e91302ad9b3ed635e075c4f0b0e
Akshat Jaiswal <akshatjaiswal872@gmail.com>
2025-07-23 15:36:47 +0530

    initial commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NotFound(){
+    return (
+        <><h1>404 NotFound</h1>
+        <p>The page you are looking is not Not Found</p> </>
+    )
+}
+export default NotFound
